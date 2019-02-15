9da4bb386aeb567d66ba7ccc8478e4f96beaa342
Dean Francis <dean@cogent.co>
2019-02-15 11:14:27 +1100

    👨‍🚀 Spin jump's velocity continues after arrow is released

--- src/axan/player.ts
+++ src/axan/player.ts
@@ -33,6 +33,7 @@ export default class Player extends Phaser.GameObjects.Sprite {
 
   // factors
   private runSpeed = 150;
+  private runVelX = 0;
   private dashSpeed = 0;
   private knockbackX = 0;
   private knockbackY = 0;
@@ -44,23 +45,23 @@ export default class Player extends Phaser.GameObjects.Sprite {
   private hurtCooldown = 900;
 
   // states
-  public isJumping = false;
   private isStandJumping = false;
   private isRunJumping = false;
+
   private isFalling = false;
   public isMoving = false;
   public isCrouching = false;
   public isRunning = false;
   private isDashing = false;
   private canHurt = true;
-  
-  
+
+
   constructor(scene, x, y, key) {
     super(scene, x, y, key);
     this.setOrigin(0.5, .8);
     this.scene.physics.world.enable(this);
     this.inventory = scene.inventory;
-    
+
     this.setDepth(10);
 
     this.keys = scene.input.keyboard.addKeys({
@@ -135,13 +136,12 @@ export default class Player extends Phaser.GameObjects.Sprite {
 
   animation(): void {
     // Run on every second frame, prevents crazy jitters
-    const {left, right, up, down} = this.inputs;
+    const { left, right, up, down } = this.inputs;
     let anim: string;
-    this.animTimer = (this.animTimer === 3) ? 0 : this.animTimer+1;
+    this.animTimer = (this.animTimer === 3) ? 0 : this.animTimer + 1;
 
     if (this.isRunning) {
       // run
-      this.isJumping = false;
       if (down) {
         anim = 'crouch-run';
       } else if (up) {
@@ -151,8 +151,7 @@ export default class Player extends Phaser.GameObjects.Sprite {
       }
     } else if (this.body.onFloor() && this.body.velocity.x === 0) {
       // crouching
-      this.isJumping = false;
-      if(down) {
+      if (down) {
         anim = 'crouch';
       } else if (up) {
         anim = 'stand-aim-up';
@@ -161,7 +160,7 @@ export default class Player extends Phaser.GameObjects.Sprite {
       } else {
         anim = 'stand';
       }
-    } else if (!this.body.onFloor() && this.isJumping) {
+    } else if (!this.body.onFloor() && this.isJumping()) {
       // airborne
       if (down && (left || right)) {
         anim = 'jump-aim-down-fwd';
@@ -172,7 +171,7 @@ export default class Player extends Phaser.GameObjects.Sprite {
       } else if (down) {
         anim = 'jump-aim-down';
       } else if (this.body.velocity.y <= 0) {
-        if(this.isRunJumping) {
+        if (this.isRunJumping) {
           anim = 'jump-spin';
         } else {
           anim = 'jump-up';
@@ -184,13 +183,13 @@ export default class Player extends Phaser.GameObjects.Sprite {
           anim = 'jump-down';
         }
       }
-    } 
+    }
 
     if (anim && this.anims.getCurrentKey() !== anim) {
       try {
         this.anims.play(anim);
       } catch {
-        console.error("Error playing "+ anim);
+        console.error("Error playing " + anim);
       }
     }
 
@@ -200,10 +199,11 @@ export default class Player extends Phaser.GameObjects.Sprite {
 
   controls(delta: number): void {
     const { left, right, up, down, shoot, jump, cycleWeapon, dash } = this.inputs;
-    
+
     if (!this.canMove) { return; }
     if (cycleWeapon) { this.nextBeam(); }
     if (this.body.onCeiling() && this.body.onFloor()) { console.log('JAMMED') }
+    if (this.body.onFloor() && this.isJumping()) { this.jumpEnd(); }
 
     this.dashSpeed = (dash) ? 150 : 0;
 
@@ -216,14 +216,14 @@ export default class Player extends Phaser.GameObjects.Sprite {
       this.beam.unShoot();
     }
 
-    if (left && !right) {
+    if (left || right) {
       this.hasMoved = true;
-      this.body.setVelocityX(-this.runSpeed + this.knockbackX + -this.dashSpeed);
-      this.flipX = true;
-    } else if (right && !left) {
-      this.hasMoved = true;
-      this.body.setVelocityX(this.runSpeed + this.knockbackX + this.dashSpeed);
-      this.flipX = false;
+      this.flipX = left ? true : false;
+      const velocityX = this.runSpeed + this.knockbackX + this.dashSpeed;
+      this.runVelX = (left) ? -velocityX : velocityX;
+      this.body.setVelocityX(this.runVelX);
+    } else if (this.isRunJumping) {
+      this.body.setVelocityX(this.runVelX);
     } else {
       this.body.setVelocityX(0 + this.knockbackX);
     }
@@ -236,6 +236,10 @@ export default class Player extends Phaser.GameObjects.Sprite {
     }
   }
 
+  isJumping(): boolean {
+    return this.isStandJumping || this.isRunJumping;
+  }
+
   jump(delta: number): void {
     const hiJumpVelocity = (this.inventory.hiJump) ? -100 : 0;
     const hiJumpTimer = (this.inventory.hiJump) ? 100 : 0;
@@ -243,15 +247,14 @@ export default class Player extends Phaser.GameObjects.Sprite {
     if (this.body.onFloor() && this.jumpTimer === 0) {
       this.jumpStart();
     } else if (this.jumpTimer > 0 && this.jumpTimer < (301 + hiJumpTimer) && !this.body.onCeiling()) {
-      this.jumpContinue(delta);
+      this.jumpRise(delta);
     } else if (this.body.onCeiling()) {
       this.jumpEnd();
     }
   }
 
   jumpStart() {
-    console.log(this.body.velocity.x);
-    if(this.body.velocity.x === 0 ) {
+    if (this.body.velocity.x === 0) {
       this.isStandJumping = true;
       this.isRunJumping = false;
     } else {
@@ -261,10 +264,9 @@ export default class Player extends Phaser.GameObjects.Sprite {
     const hiJumpVelocity = (this.inventory.hiJump) ? -100 : 0;
     this.jumpTimer = 1;
     this.body.setVelocityY(-150 + hiJumpVelocity);
-    this.isJumping = true;
   }
 
-  jumpContinue(delta: number) {
+  jumpRise(delta: number) {
     const hiJumpVelocity = (this.inventory.hiJump) ? -100 : 0;
     this.jumpTimer += delta;
     this.body.setVelocityY(-160 + hiJumpVelocity);
@@ -296,7 +298,7 @@ export default class Player extends Phaser.GameObjects.Sprite {
       const nextBeam = this.scene.inventory.nextBeam();
       this.changeBeam(nextBeam);
       this.switchWeaponTimer = 1;
-      
+
       this.scene.time.addEvent({
         delay: 300,
         callbackScope: this,
@@ -366,7 +368,6 @@ export default class Player extends Phaser.GameObjects.Sprite {
           this.canHurt = true;
         }
       });
-
     }
   }
 
@@ -385,7 +386,5 @@ export default class Player extends Phaser.GameObjects.Sprite {
         this.knockbackY = 0;
       }
     });
-
   }
-
 }
