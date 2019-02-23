bea468fbc12ecc4290ac3f24a9b7dcb56a1e66e0
Dean Francis <dean@cogent.co>
2019-02-23 17:53:07 +1100

    🛁 Pulling animation data into a big messy file

--- src/axan/main.scene.ts
+++ src/axan/main.scene.ts
@@ -7,6 +7,7 @@ import HUD from "./hud";
 import RoomVisibility from "./rooms/room-visibility";
 import RandomPlanetName from "../util/name-gen";
 import { Enemy } from "axan/enemies";
+import MakeAnimations from "./animations";
 import Projectile from "axan/beams/projectile";
 import * as _ from "lodash";
 
@@ -60,6 +61,8 @@ export default class MainScene extends Phaser.Scene {
     console.log("Welcome to " + this.levelName);
     this.level = new Level(this);
     this.inventory = new Inventory(this);
+    
+    MakeAnimations(this);
 
     this.makeTiles();
     this.makeBackTiles();
@@ -80,9 +83,7 @@ export default class MainScene extends Phaser.Scene {
     this.player.update(time, delta);
 
     this.enemyGroup.children.entries.concat(this.killedEnemies.children.entries)
-      .forEach(
-        enemy => enemy.update(time, delta), this
-      );
+      .forEach(enemy => enemy.update(time, delta), this);
   }
 
   makeTiles() {
@@ -156,34 +157,6 @@ export default class MainScene extends Phaser.Scene {
   }
   
   setupDoorGateGroup() {
-    [
-      {
-        key: 'door-open',
-        repeat: 0,
-        frameRate: 20,
-        frames: this.anims.generateFrameNames('door-gates', { start: 1, end: 3, prefix: 'door-open' })
-      }, {
-        key: 'door-close',
-        repeat: 0,
-        frameRate: 20,
-        frames: 
-          this.anims.generateFrameNames('door-gates', { start: 1, end: 3, prefix: 'door-open' }).reverse()
-            .concat(this.anims.generateFrameNames('door-gates', { start: 1, end: 1, prefix: 'door-active' }))
-            .concat(this.anims.generateFrameNames('door-gates', { start: 1, end: 1, prefix: 'door-active' }))
-            .concat(this.anims.generateFrameNames('door-gates', { start: 1, end: 1, prefix: 'door-active' }))
-            .concat(this.anims.generateFrameNames('door-gates', { start: 1, end: 1, prefix: 'door-active' }))
-            .concat(this.anims.generateFrameNames('door-gates', { start: 1, end: 1, prefix: 'door-closed' }))
-      }, {
-        key: 'door-closed',
-        repeat: -1,
-        frames: this.anims.generateFrameNames('door-gates', { start: 1, end: 1, prefix: 'door-closed' })
-      }, {
-        key: 'door-active',
-        repeat: -1,
-        frames: this.anims.generateFrameNames('door-gates', { start: 1, end: 1, prefix: 'door-active' })
-      }
-    ].forEach(anim => this.anims.create(anim));
-
     this.doorGateGroup = this.add.group();
     // enemy / door gate hit detection
     this.physics.add.collider(this.enemyGroup, this.doorGateGroup);
@@ -196,56 +169,6 @@ export default class MainScene extends Phaser.Scene {
 
   setupPickupGroup() {
     this.pickupGroup = this.add.group();
-
-    const repeat = -1;
-    const frameRate = 15;
-
-    [
-      {
-        key: 'health-pickup',
-        frameRate: 7,
-        yoyo: true,
-        frames: this.anims.generateFrameNames('pickups', { start: 1, end: 3, prefix: "health-pickup" })
-      }, {
-        key: 'hi-jump-boots',
-        frameRate: 7,
-        frames: this.anims.generateFrameNames('pickups', { start: 1, end: 2, prefix: "boot-pickup" })
-      }, {
-        key: 'dash-boots',
-        frameRate: 7,
-        frames: this.anims.generateFrameNames('pickups', { start: 1, end: 2, prefix: "dash-pickup" })
-      }, {
-        key: 'wall-jump-boots',
-        frameRate: 7,
-        frames: this.anims.generateFrameNames('pickups', { start: 1, end: 2, prefix: "walljump-pickup" })
-      }, {
-        key: 'health-upgrade',
-        frameRate: 7,
-        frames: this.anims.generateFrameNames('pickups', { start: 1, end: 2, prefix: "globe" })
-      }, {
-        key: 'health-upgrade-2',
-        frameRate: 7,
-        frames: this.anims.generateFrameNames('pickups', { start: 1, end: 2, prefix: "globe-steel" })
-      }, {
-        key: 'pickup-holder',
-        frames: this.anims.generateFrameNames('pickups', { start: 1, end: 1, prefix: "pickup-holder" })
-      }, {
-        key: 'charge',
-        frames: this.anims.generateFrameNames('beam-pickups', { start: 0, end: 1 })
-      }, {
-        key: 'spazer',
-        frames: this.anims.generateFrameNames('beam-pickups', { start: 2, end: 3 })
-      }, {
-        key: 'wave',
-        frames: this.anims.generateFrameNames('beam-pickups', { start: 4, end: 5 })
-      }, {
-        key: 'ice',
-        frames: this.anims.generateFrameNames('beam-pickups', { start: 6, end: 7 })
-      }, {
-        key: 'plasma',
-        frames: this.anims.generateFrameNames('beam-pickups', { start: 8, end: 9 })
-      }
-    ].forEach(anim => this.anims.create(_.merge({ repeat, frameRate }, anim)));
   }
 
   setupProjectileGroup() {
@@ -264,38 +187,6 @@ export default class MainScene extends Phaser.Scene {
 
     this.physics.add.overlap(this.projectileGroup, this.enemyGroup, this.enemyShot, undefined, this);
     this.physics.add.overlap(this.projectileGroup, this.doorGateGroup, this.doorShot, undefined, this);
-
-    [
-      {
-        key: 'beam-photon',
-        frames: this.anims.generateFrameNames('projectiles', { start: 1, end: 1, prefix: "photon", zeroPad: 2 })
-      },
-      {
-        key: 'beam-pulse',
-        frames: this.anims.generateFrameNames('projectiles', { start: 1, end: 1, prefix: "pulse", zeroPad: 2 })
-      },
-      {
-        key: 'beam-rang',
-        frames: this.anims.generateFrameNames('projectiles', { start: 1, end: 1, prefix: "rang", zeroPad: 2 })
-      },
-      {
-        key: 'beam-orb',
-        frames: this.anims.generateFrameNames('projectiles', { start: 1, end: 1, prefix: "orb", zeroPad: 2 })
-      },
-      {
-        key: 'beam-ice',
-        frames: this.anims.generateFrameNames('projectiles', { start: 1, end: 1, prefix: "ice", zeroPad: 2 })
-      },
-      {
-        key: 'beam-fire',
-        frames: this.anims.generateFrameNames('projectiles', { start: 1, end: 1, prefix: "fire", zeroPad: 2 })
-      }, {
-        key: 'beam-impact',
-        frameRate: 20,
-        repeat: 0,
-        frames: this.anims.generateFrameNames('effects', { start: 0, end: 5, prefix: 'beam-impact' })
-      }
-    ].forEach(anim => this.anims.create(anim))
   }
 
   setupEnemyGroup() {
@@ -305,43 +196,6 @@ export default class MainScene extends Phaser.Scene {
     // world / enemy hit detection
     this.physics.add.collider(this.enemyGroup, this.groundLayer);
     this.physics.add.collider(this.killedEnemies, this.groundLayer);
-
-    [{
-      key: 'piq',
-      frameRate: 15,
-      repeat: -1,
-      frames: this.anims.generateFrameNames('enemies', { start: 0, end: 4, prefix: 'piq', zeroPad: 2 })
-    }, {
-      key: 'gnid',
-      frameRate: 15,
-      repeat: -1,
-      frames: this.anims.generateFrameNames('enemies', { start: 1, end: 8, prefix: 'gnid' })
-    }, {
-      key: 'jumper-idle',
-      frameRate: 4,
-      repeat: -1,
-      frames: this.anims.generateFrameNames('enemies', { start: 0, end: 4, prefix: 'jumper-idle', zeroPad: 2 })
-    }, {
-      key: 'jumper-jump',
-      frameRate: 4,
-      repeat: 0,
-      frames: this.anims.generateFrameNames('enemies', { start: 1, end: 1, prefix: 'jumper-jump', zeroPad: 2 })
-    }, {
-      key: 'vroll',
-      frameRate: 4,
-      repeat: -1,
-      frames: this.anims.generateFrameNames('enemies', { start: 0, end: 2, prefix: 'vroll-idle', zeroPad: 2 })
-    }, {
-      key: 'vroll-down',
-      frameRate: 4,
-      repeat: -1,
-      frames: this.anims.generateFrameNames('enemies', { start: 0, end: 2, prefix: 'vroll-attack', zeroPad: 2 })
-    }, {
-      key: 'enemy-death',
-      frameRate: 20,
-      repeat: 0,
-      frames: this.anims.generateFrameNames('effects', { start: 0, end: 6, prefix: 'enemy-death', zeroPad: 2 })
-    }].forEach(anim => this.anims.create(anim))
   }
 
   // refactor into enemy.ts
