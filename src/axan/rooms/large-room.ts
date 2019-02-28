c986cce6a0a444ab53e6048073e6c4a1cda624ea
Dean Francis <dean@cogent.co>
2019-03-01 08:14:05 +1100

    🐞 Turns out large rooms just werent working

--- src/axan/rooms/large-room.ts
+++ src/axan/rooms/large-room.ts
@@ -1,6 +1,6 @@
 import { Room } from ".";
 import Wall from "../tiles/wall";
-import Cellular from "cellular-dungeon";
+import * as Cellular from "cellular-dungeon";
 
 export class LargeRoom extends Room {
   constructor(room, scene) {
@@ -9,14 +9,18 @@ export class LargeRoom extends Room {
   }
 
   instantiatePlatforms() {
-    const cellularMap = Cellular(this.room.width, this.room.height, { aliveThreshold: 4.7 });
-    cellularMap.grid.forEach((tiles, y) => {
-      tiles.map((place, x) => {
-        if (place) {
+    const { width: roomWidth, height: roomHeight } = this.room;
+    const cellularMap = Cellular(this.room.width, this.room.height, { aliveThreshold: 4.12 });
+
+    for (let y = 1; y < (roomHeight - 1); y++) {
+      for (let x = 1; x < (roomWidth - 1); x++) {
+        const cellularMapTile = cellularMap.grid[y][x];
+        if (cellularMapTile) {
           this.tiles[y][x] = new Wall(x, y, this);
         }
-      });
-    });
+      }
+    }
+    this.clearDoorways();
   }
 
 }
