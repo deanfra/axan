7241b9b38fb42bef7b9705b57880549e52a79d30
Dean Francis <dean.francis@rea-group.com>
2022-01-13 06:23:48 +1100

    🚧 Dependency bumps for the year 202X

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,71 @@
 // @ts-check
 
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 
-const webpack = require('webpack');
-const path = require('path');
+const webpack = require("webpack");
+const path = require("path");
 
 module.exports = {
   devServer: {
-    host: '0.0.0.0'
+    host: "0.0.0.0",
   },
   entry: {
-    main: path.resolve(__dirname, 'src/index.ts')
+    main: path.resolve(__dirname, "src/index.ts"),
   },
   resolve: {
-    extensions: ['.ts', '.js'],
-    plugins: [new TsconfigPathsPlugin()]
+    extensions: [".ts", ".js"],
+    plugins: [new TsconfigPathsPlugin()],
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: 'ts-loader'
+        use: "ts-loader",
       },
       {
         test: [/\.vert$/, /\.frag$/],
-        use: 'raw-loader'
+        use: "raw-loader",
       },
       {
-        test: [
-          /\.(png|fnt|mp3|ogg)$/
+        test: [/\.(png|fnt|mp3|ogg)$/],
+        use: [
+          {
+            loader: "file-loader",
+            options: { name: "assets/[name].[hash].[ext]" },
+          },
         ],
-        use: [{ loader: 'file-loader', options: { name: 'assets/[name].[hash].[ext]' } }]
       },
       {
-        type: 'javascript/auto',
+        type: "javascript/auto",
         test: /\.json$/,
-        use: [{ loader: 'file-loader', options: { name: 'assets/[name].[hash].[ext]' } }]
-      }
-    ]
+        use: [
+          {
+            loader: "file-loader",
+            options: { name: "assets/[name].[hash].[ext]" },
+          },
+        ],
+      },
+    ],
   },
   optimization: {
     splitChunks: {
       cacheGroups: {
         commons: {
           test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all'
-        }
-      }
-    }
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
   },
   plugins: [
     new webpack.DefinePlugin({
       WEBGL_RENDERER: true,
-      CANVAS_RENDERER: true
+      CANVAS_RENDERER: true,
     }),
     new HtmlWebPackPlugin({
-      template: './src/index.html'
-    })
-  ]
-}
+      template: "./src/index.html",
+    }),
+  ],
+};
